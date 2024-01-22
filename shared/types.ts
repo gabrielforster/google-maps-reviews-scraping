c486b9ac9e094490c3de895c9f4fa0e7250eee1c
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-01-22 18:09:33 -0300

    feat: adding Reviews types

--- shared/types.ts
+++ shared/types.ts
@@ -1,7 +1,12 @@
 export type Review = {
-  review: number;
-  comment: string;
+  placeId: string;
+  rating: number;
+  comment?: string | null;
   reviewer: string;
   link: string;
   dateTimestamp: number;
+}
+
+export type ReviewWithId = Review & {
+  id: string;
 }

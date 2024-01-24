ea163d09cfe3fc347ed75981ff6384c7d4b2462f
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-01-24 13:26:52 -0300

    feat: remove comments and fix code indent

--- scraper/ports/processor.ts
+++ scraper/ports/processor.ts
@@ -1,11 +1,19 @@
 import { Lambda } from "@aws-sdk/client-lambda";
 import puppeteer, { HTTPRequest } from 'puppeteer-core';
 import chromium from '@sparticuz/chromium';
-import fs from 'fs';
 
 const REVIEWS_URL = "https://www.google.com/maps/rpc/listugcposts";
 
-  type Input = {
+  const lambda = new Lambda({
+  ...(
+    process.env.IS_OFFLINE
+      ? { region: 'localhost', endpoint: `http://localhost:${process.env.scraperPort}` }
+      : {}
+  ),
+})
+
+
+type Input = {
   url: string
   placeId: string
 }
@@ -14,7 +22,6 @@ export async function handler (input: Input) {
   const { url, placeId } = input;
 
   try {
-
     const path = process.env.IS_OFFLINE
       ? undefined
       : "/opt/nodejs/node_modules/@sparticuz/chromium/bin"
@@ -28,154 +35,108 @@ export async function handler (input: Input) {
       protocolTimeout: 450_000,
     });
 
-      const page = await browser.newPage();
+    const page = await browser.newPage();
 
-      await page.evaluateOnNewDocument(() => {
-        Object.defineProperty(navigator, "language", {
-          get: function() {
-            return "en-US";
-          }
-        });
-        Object.defineProperty(navigator, "languages", {
-          get: function() {
-            return ["en-US", "en"];
-          }
-        });
-      });
+    await page.goto(url);
 
-      await page.setExtraHTTPHeaders({
-        'Accept-Language': 'en-US'
-      });
+    await page.waitForSelector("div[role='tablist']");
 
-      await page.goto(url);
+    await page.evaluate(() => {
+      const buttonsWrapper = document.querySelector("div[role='tablist']")
+      if (!buttonsWrapper) {
+        throw new Error("Buttons wrapper not found");
+      }
 
-      await page.waitForSelector("div[role='tablist']");
+      const button = Array.from(buttonsWrapper.children)[1] as HTMLButtonElement | null
 
-      await page.evaluate(() => {
-        const buttonsWrapper = document.querySelector("div[role='tablist']")
-        if (!buttonsWrapper) {
-          throw new Error("Buttons wrapper not found");
-        }
+      if (!button) {
+        throw new Error("Reviews button not found");
+      }
 
-        // const button = Array.from(buttonsWrapper.children)
-        //   .find(button => button.innerHTML.includes("Reviews")) as HTMLButtonElement | null
+      button.click();
+    })
 
-        const button = Array.from(buttonsWrapper.children)[1] as HTMLButtonElement | null
+    const reviewsData: any[] = [];
 
-        if (!button) {
-          throw new Error("Reviews button not found");
-        }
-
-        button.click();
-      })
-
-      const reviewsCount = await page.evaluate(() => {
-        const wrapper = Array.from(document.getElementsByClassName("fontBodySmall"))
-
-        return wrapper[0].innerHTML.split(" ")[0];
-
-        // const reviewsText = wrapper.find((element) => element.innerHTML.includes("reviews"))
-        // if (!reviewsText) {
-        //   throw new Error("Reviews text not found");
-        // }
-        //
-        // const [count] = reviewsText.innerHTML.split(" ");
-        // return count;
-
-      });
-
-      const reviewsData: any[] = [];
-
-      await new Promise<void>(async (resolveMain) => {
-        page.setRequestInterception(true);
+    await new Promise<void>(async (resolveMain) => {
+      page.setRequestInterception(true);
 
 
-        page.on('request', (request: HTTPRequest) => {
-          if (request.url().startsWith(REVIEWS_URL)) {
-            reviewsData.push({
-              url: request.url(),
-              headers: request.headers(),
-            })
-          }
-
-          request.continue();
-        })
-
-        // await page.waitForSelector("button[aria-label='Sort reviews'");
-        await page.waitForSelector("button[aria-label='Classificar avaliações'");
-
-          await page.evaluate(async () => {
-            // const sortButton = document.querySelector("button[aria-label='Sort reviews'") as HTMLButtonElement | null
-            const sortButton = document.querySelector("button[aria-label='Classificar avaliações'") as HTMLButtonElement | null
-              if (!sortButton) {
-                throw new Error("Sort button not found");
-              }
-
-              sortButton.click();
-
-              await new Promise(resolve => setTimeout(resolve, 5000));
-
-              const optionsMenu = document.querySelector("div[id='action-menu'")
-                if (!optionsMenu) {
-                  throw new Error("Options menu not found");
-                }
-
-                const mostRelevantOption = Array.from(optionsMenu.children)
-                // .find(element => element.innerHTML.includes("Most relevant")) as HTMLButtonElement | null
-                .find(element => element.innerHTML.includes("Mais relevantes")) as HTMLButtonElement | null
-                if (!mostRelevantOption) {
-                  throw new Error("Most relevant option not found");
-                }
-
-                mostRelevantOption.click();
-
-                await new Promise(resolve => setTimeout(resolve, 500));
-
-                // const reviewsWrapper = sortButton.parentElement?.parentElement?.parentElement
-                const reviewsWrapper = document.querySelector("div[role='main']")?.children[1]
-                if (!reviewsWrapper) {
-                  throw new Error("Reviews wrapper not found");
-                }
-
-                let hasChanged = true;
-
-                while (hasChanged) {
-                  const currentLastReview = reviewsWrapper.children[8].children[reviewsWrapper.children[8].children.length - 1];
-
-                  reviewsWrapper.scrollTo(0, reviewsWrapper.scrollHeight);
-
-                  await new Promise(resolve => setTimeout(resolve, 6000));
-
-                  const newLastReview = reviewsWrapper.children[8].children[reviewsWrapper.children[8].children.length - 1];
-
-                  hasChanged = currentLastReview !== newLastReview;
-                }
+      page.on('request', (request: HTTPRequest) => {
+        if (request.url().startsWith(REVIEWS_URL)) {
+          reviewsData.push({
+            url: request.url(),
+            headers: request.headers(),
           })
+        }
 
-          resolveMain();
+        request.continue();
       })
 
-      const lambda = new Lambda({
-        ...(
-          process.env.IS_OFFLINE
-            ? { region: 'localhost', endpoint: `http://localhost:${process.env.scraperPort}` }
-            : {}
-        ),
+      await page.waitForSelector("button[aria-label='Classificar avaliações'");
+
+      await page.evaluate(async () => {
+        const sortButton = document.querySelector("button[aria-label='Classificar avaliações'") as HTMLButtonElement | null
+        if (!sortButton) {
+          throw new Error("Sort button not found");
+        }
+
+        sortButton.click();
+
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        const optionsMenu = document.querySelector("div[id='action-menu'")
+        if (!optionsMenu) {
+          throw new Error("Options menu not found");
+        }
+
+        const mostRelevantOption = Array.from(optionsMenu.children)
+          .find(element => element.innerHTML.includes("Mais relevantes")) as HTMLButtonElement | null
+        if (!mostRelevantOption) {
+          throw new Error("Most relevant option not found");
+        }
+
+        mostRelevantOption.click();
+
+        await new Promise(resolve => setTimeout(resolve, 500));
+
+        const reviewsWrapper = document.querySelector("div[role='main']")?.children[1]
+        if (!reviewsWrapper) {
+          throw new Error("Reviews wrapper not found");
+        }
+
+        let hasChanged = true;
+
+        while (hasChanged) {
+          const currentLastReview = reviewsWrapper.children[8].children[reviewsWrapper.children[8].children.length - 1];
+
+          reviewsWrapper.scrollTo(0, reviewsWrapper.scrollHeight);
+
+          await new Promise(resolve => setTimeout(resolve, 6000));
+
+          const newLastReview = reviewsWrapper.children[8].children[reviewsWrapper.children[8].children.length - 1];
+
+          hasChanged = currentLastReview !== newLastReview;
+        }
       })
 
-      await lambda.invoke({
-        FunctionName: 'scraper-api-mapper',
-        InvocationType: 'Event',
-        Payload: JSON.stringify({
-          data: reviewsData,
-          placeId
-        }),
-      });
+      resolveMain();
+    })
+
+    await lambda.invoke({
+      FunctionName: 'scraper-api-mapper',
+      InvocationType: 'Event',
+      Payload: JSON.stringify({
+        data: reviewsData,
+        placeId
+      }),
+    });
   } catch (error) {
     console.info("failed", {
       placeId,
       url
     });
+
     console.error(error);
   }
 };

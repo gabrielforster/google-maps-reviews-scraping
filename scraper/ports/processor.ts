6dc90e83b9cf2ea7c377589326bab49714fc7f66
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-01-24 03:16:31 -0300

    feat: increase timeout on wait for reviews wrapper

--- scraper/ports/processor.ts
+++ scraper/ports/processor.ts
@@ -123,7 +123,7 @@ export async function handler (input: Input) {
 
                 mostRelevantOption.click();
 
-                await new Promise(resolve => setTimeout(resolve, 1000));
+                await new Promise(resolve => setTimeout(resolve, 2000));
 
                 const reviewsWrapper = sortButton.parentElement?.parentElement?.parentElement
                 if (!reviewsWrapper) {
